4b4eafe538a62eba514c0a14c397225117f527bd
martinsalinas0 <martinlsalinas@pm.me>
2025-02-15 15:25:14 -0600

    added renderBooks function

--- main.js
+++ main.js
@@ -12,8 +12,31 @@ const books = [
 let renderBooks = function (book){ 
   // my code here 
   document.querySelector('.books').replaceChildren(); 
+
+  for (let i = 0; i < books.length; i++){ 
+    const book = books[i]
+
+
+    const template = `
+    <div class="book col-md-6">
+      <h4>${book.title}</h4>
+      <div>Author: <strong>${book.author}</strong></div>
+      <div>Pages: <strong>${book.pageCount}</strong></div>
+      <div>ISBN: <strong>${book.isbn}</strong></div>
+      <img src="${book.imageURL}" alt="">
+      </div>`;
+
+      document.querySelector('.books').insertAdjacentHTML('beforeend', template)
+
+  }
+
+
+
+
 }
 
 
 
 renderBooks(); 
+
+
