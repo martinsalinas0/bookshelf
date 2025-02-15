2fa5a628b28e62dbb1a6bf07267db882e2e8d98d
martinsalinas0 <martinlsalinas@pm.me>
2025-02-15 17:40:27 -0600

    added render books and add books and fetvh

--- main.js
+++ main.js
@@ -1,39 +1,69 @@
-const books = [
-  {
-    title: 'Harry Potter',
-    author: 'J.K. Rowling',
-    imageURL: 'https://books.google.com/books/content?id=WV8pZj_oNBwC&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api',
-    isbn: '9781921479311',
-    pageCount: 268
-  }
-]; 
+let books = []
 
 
-let renderBooks = function (book){ 
-  // my code here 
-  document.querySelector('.books').replaceChildren(); 
+document.querySelector('.search').addEventListener('click', function () {
+  const search = document.querySelector('#search-query').value;
 
-  for (let i = 0; i < books.length; i++){ 
-    const book = books[i]
+  fetchBooks(search);
 
+  document.querySelector('#search-query').value = '';
+});
 
-    const template = `
-    <div class="book col-md-6">
-      <h4>${book.title}</h4>
-      <div>Author: <strong>${book.author}</strong></div>
-      <div>Pages: <strong>${book.pageCount}</strong></div>
-      <div>ISBN: <strong>${book.isbn}</strong></div>
-      <img src="${book.imageURL}" alt="">
-      </div>`;
-
-      document.querySelector('.books').insertAdjacentHTML('beforeend', template)
-
-  }
-
-
-
-
+const fetchBooks = (query) => {
+  const url = 'https://www.googleapis.com/books/v1/volumes?q=' + query;
+  fetch(url, {
+    method: 'GET',
+    dataType: 'json'
+  })
+    .then(data => data.json())
+    .then(data => addBooks(data));
 }
+
+const addBooks = (data) => {
+  books = [];
+
+  for (let i = 0; i < data.items.length; i++) {
+    const bookData = data.items[i];
+
+    const book = {
+      title: bookData.volumeInfo.title || null,
+      author: bookData.volumeInfo.authors ? bookData.volumeInfo.authors[0] : null,
+      imageURL: bookData.volumeInfo.imageLinks ? bookData.volumeInfo.imageLinks.thumbnail : null,
+      pageCount: bookData.volumeInfo.pageCount || null,
+      isbn: bookData.volumeInfo.industryIdentifiers ?
+        bookData.volumeInfo.industryIdentifiers[0].identifier : null
+    };
+
+    books.push(book);
+  }
+
+  renderBooks();
+
+};
+
+
+
+const renderBooks = () => {
+  document.querySelector('.books').replaceChildren();
+  
+    for (let i = 0; i < books.length; i++) {
+      const book = books[i];
+  
+      const template = `
+      <div class="book col-md-6">
+        <h4>${ book.title }</h4>
+        <div>Author: <strong>${ book.author }</strong></div>
+        <div>Pages: <strong>${ book.pageCount }</strong></div>
+        <div>ISBN: <strong>${ book.isbn }</strong></div>
+        <img src="${book.imageURL}" alt="">
+      </div>`;
+  
+      document.querySelector('.books').insertAdjacentHTML('beforeend', template);
+  
+    }
+  
+  };
+  
 
 
 
